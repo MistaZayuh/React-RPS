85148fcf126fffece397533ab19d576db809d14a
Isaiah Boren <MistaZayuh@gmail.com>
2019-09-11 19:08:42 -0600

    game is functional

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Header, Container, Statistic, Button, Icon } from "semantic-ui-react";
 // import Results from "./Results";
 
 class App extends React.Component {
-  state = { history: { wins: 0, losses: 0, ties: 0}, winner: "banana" };
+  state = { wins: 0, losses: 0, ties: 0, winner: null };
 
   handleClick = (name) => {
     let i = this.rando()
@@ -24,21 +24,23 @@ class App extends React.Component {
       this.setState({winner: "player"});}
     else if (comp === "scissors" && name === "paper"){
       this.setState({winner: "computer"});}
-      setHistory()
+      this.setHistory()
 
   };
 
   rando = () => {
-    return Math.floor((Math.random()) * 2);
+    return Math.floor((Math.random()) * 3);
   };
 
   setHistory = () => {
     if (this.state.winner === "player" ) {
-      this.setState({history: [wins: (this.state.history.wins +1)]})
+      this.setState({ wins: this.state.wins + 1})
     } else if (this.state.winner === "computer" ) {
-      this.setState({history: [wins: (this.state.history.wins +1)]})
-    }
-  }
+      this.setState({ losses: this.state.losses + 1})
+    } else if (this.state.winner === "none" ) {
+    this.setState({ ties: this.state.ties + 1})
+    };
+  };
 
   render() {
     return(
@@ -49,15 +51,15 @@ class App extends React.Component {
         <div>
     <Statistic.Group>
       <Statistic>
-        <Statistic.Value>{this.state.history.wins}</Statistic.Value>
+        <Statistic.Value>{this.state.wins}</Statistic.Value>
         <Statistic.Label>Wins</Statistic.Label>
       </Statistic>
       <Statistic>
-        <Statistic.Value>{this.state.history.losses}</Statistic.Value>
+        <Statistic.Value>{this.state.losses}</Statistic.Value>
         <Statistic.Label>Losses</Statistic.Label>
       </Statistic>
       <Statistic>
-        <Statistic.Value>{this.state.history.ties}</Statistic.Value>
+        <Statistic.Value>{this.state.ties}</Statistic.Value>
         <Statistic.Label>Ties</Statistic.Label>
       </Statistic>
     </Statistic.Group>
